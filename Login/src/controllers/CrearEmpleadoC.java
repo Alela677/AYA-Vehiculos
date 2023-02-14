fe7e8e679d22b7014449b6c973ce33b904853cb7
Alejandro <alejandrolealastorga@gmail.com>
2023-02-14 12:57:39 +0100

    Pantalla completa

---
+++ Login/src/controllers/CrearEmpleadoC.java
@@ -0,0 +1,149 @@
+package controllers;
+
+import java.awt.Component;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+
+import javax.swing.JFileChooser;
+
+import org.hibernate.Session;
+
+import daos.EmpleadosDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import models.Empleados;
+import models.HibernateUtil;
+import utils.GuardarImagen;
+import utils.HashPassword;
+
+public class CrearEmpleadoC implements Initializable {
+
+	private Session sesion = HibernateUtil.getSession();
+	EmpleadosDAO gestorEmpleados = new EmpleadosDAO(sesion);
+
+	final FileChooser fileChooser = new FileChooser();
+
+	@FXML
+	private Button buscarImagen;
+
+	private Component nombreImagen;
+
+	@FXML
+	private TextField txtArchivoImg;
+
+	@FXML
+	private TextField txtNombre;
+
+	@FXML
+	private TextField txtApellidos;
+
+	@FXML
+	private TextField txtDni;
+
+	@FXML
+	private TextField txtContraseña;
+
+	@FXML
+	private DatePicker boxFechaAlta;
+
+	@FXML
+	private ComboBox<String> comboDepartamento;
+
+	@FXML
+	private ComboBox<String> comboCargo;
+
+	@FXML
+	void buscarImagen(MouseEvent event) throws Exception {
+		JFileChooser fc = new JFileChooser(); // Creamos un nuevo objeto JFileChooser
+		int seleccion = fc.showOpenDialog(this.nombreImagen); // Abrimos el JFileChooser y guardamos el resultado en
+																// seleccion
+		if (seleccion == JFileChooser.APPROVE_OPTION) { // Si el usuario ha pulsado la opción Aceptar
+			File fichero = fc.getSelectedFile(); // Guardamos en la variable fichero el archivo seleccionado
+
+			if (fichero.getName().contains(".jpg") || fichero.getName().contains(".png")
+					|| fichero.getName().contains(".jpeg")) {
+				txtArchivoImg.setText(fichero.getName());
+				GuardarImagen.leerImagen(fichero);
+			}
+
+		}
+
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		Image img = new Image("images/lupa.png");
+		ImageView view = new ImageView(img);
+		view.setFitHeight(12);
+		view.setFitWidth(12);
+		buscarImagen.setGraphic(view);
+
+		rellenarCamposCargo();
+		rellenarCamposDepartamento();
+	}
+
+	@FXML
+	void borrarCamposFormulario(MouseEvent event) throws IOException {
+		actualizarPagina();
+
+	}
+
+	@FXML
+	void crearEmpleado(MouseEvent event) throws Exception {
+
+		String nombre = txtNombre.getText();
+		String apellidos = txtApellidos.getText();
+		String dni = txtDni.getText();
+		String departamento = comboDepartamento.getValue();
+		String cargo = comboCargo.getValue();
+		String fechaAlta = String.valueOf(boxFechaAlta.getValue());
+		String imagen = txtArchivoImg.getText();
+		String contraseña = txtContraseña.getText();
+
+		if (imagen.isEmpty()) {
+			imagen = "user.jpeg";
+		}
+		Empleados nuevoEmpleado = new Empleados(nombre, apellidos, dni, departamento, cargo, fechaAlta, imagen,
+				HashPassword.convertirSHA256(contraseña));
+
+		gestorEmpleados.insert(nuevoEmpleado);
+		actualizarPagina();
+	}
+
+	private void rellenarCamposCargo() {
+		List<String> cargos = gestorEmpleados.traerValoresColumnasCargos();
+		cargos = cargos.stream().distinct().collect(Collectors.toList());
+		ObservableList<String> items = FXCollections.observableArrayList(cargos);
+		comboCargo.setItems(items);
+	}
+
+	private void rellenarCamposDepartamento() {
+		ObservableList<String> items = FXCollections.observableArrayList();
+		items.add("Ventas");
+		items.add("Mecanico");
+		comboDepartamento.setItems(items);
+	}
+
+	private void actualizarPagina() throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/Views/SceneCrearEmpleado.fxml"));
+		AnchorPane root = loader.load();
+		LoginController.root.setCenter(root);
+	}
+}
---
+++ Login/src/controllers/ReparacionesC.java
@@ -0,0 +1,114 @@
+package controllers;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.hibernate.Session;
+
+import daos.ReparacionDAO;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import models.HibernateUtil;
+import models.Reparacion;
+import models.TablaRankingMecanico;
+import models.Venta;
+
+public class ReparacionesC implements Initializable {
+
+	private Session sesion = HibernateUtil.getSession();
+	ReparacionDAO gestorReparacion = new ReparacionDAO(sesion);
+
+	List<Reparacion> listaReparaciones = new ArrayList<Reparacion>();
+	static ObservableList<Reparacion> reparaciones = null;
+
+	List<Object[]> listaRanking = null;
+	static ObservableList<TablaRankingMecanico> rankingMecanico = null;
+
+	@FXML
+	private TableView<Reparacion> tablaReparaciones;
+
+	@FXML
+	private TableColumn<Reparacion, Integer> coID;
+
+	@FXML
+	private TableColumn<Reparacion, String> colMecanico;
+
+	@FXML
+	private TableColumn<Reparacion, String> colMatricula;
+
+	@FXML
+	private TableColumn<Reparacion, String> colPieza;
+
+	@FXML
+	private TableColumn<Reparacion, String> colFecha;
+
+	@FXML
+	private TableColumn<Reparacion, Double> colCoste;
+
+	@FXML
+	private TableView<TablaRankingMecanico> tablaRanking;
+
+	@FXML
+	private TableColumn<TablaRankingMecanico, Integer> colIdMencanico;
+
+	@FXML
+	private TableColumn<TablaRankingMecanico, String> colNombreMecanico;
+
+	@FXML
+	private TableColumn<TablaRankingMecanico, Integer> colReparacinoesMecanicos;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		listaReparaciones = gestorReparacion.searchAll("Reparacion");
+		ObservableList<Reparacion> reparaciones = FXCollections.observableArrayList(listaReparaciones);
+
+		añadirFilasVentas(reparaciones);
+		añadirFilasRanking();
+		columnasResizable(false);
+	}
+
+	private void añadirFilasVentas(ObservableList<Reparacion> ventasLista) {
+		coID.setCellValueFactory(new PropertyValueFactory<>("idReparacion"));
+		colMecanico.setCellValueFactory(new PropertyValueFactory<>("nombreMecanico"));
+		colMatricula.setCellValueFactory(new PropertyValueFactory<>("matricula"));
+		colPieza.setCellValueFactory(new PropertyValueFactory<>("pieza"));
+		colCoste.setCellValueFactory(new PropertyValueFactory<>("coste"));
+		colFecha.setCellValueFactory(new PropertyValueFactory<>("fecha"));
+		tablaReparaciones.getItems().addAll(ventasLista);
+	}
+
+	private void añadirFilasRanking() {
+
+		listaRanking = gestorReparacion.consultarRankingReparacion();
+		rankingMecanico = FXCollections.observableArrayList();
+
+		for (Object[] objects : listaRanking) {
+			rankingMecanico.add(new TablaRankingMecanico((int) objects[0], (String) objects[1], (long) objects[2]));
+		}
+
+		colIdMencanico.setCellValueFactory(new PropertyValueFactory<>("id"));
+		colNombreMecanico.setCellValueFactory(new PropertyValueFactory<>("nombre"));
+		colReparacinoesMecanicos.setCellValueFactory(new PropertyValueFactory<>("totalReparacion"));
+		tablaRanking.getItems().addAll(rankingMecanico);
+	}
+
+	private void columnasResizable(boolean opc) {
+		colCoste.setResizable(opc);
+		coID.setResizable(opc);
+		colFecha.setResizable(opc);
+		colMatricula.setResizable(opc);
+		colMecanico.setResizable(opc);
+		colPieza.setResizable(opc);
+
+	}
+
+}
